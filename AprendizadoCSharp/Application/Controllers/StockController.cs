7efe49b51374af329a603747b33b69ecece3290d
JoaoPauloLordelo <joao.lordelo@souunit.com.br>
2026-08-17 23:05:23 -0300

    feat(application): Remoção de controller existente e criação do StockController

---
+++ AprendizadoCSharp/Application/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+﻿using AprendizadoCSharp.Domain.Stock.Models;
+using AprendizadoCSharp.Infrasctucture.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AprendizadoCSharp.Application.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext ApplicationDBContext)
+        {
+            this._context = ApplicationDBContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStocks()
+        {
+            List<Stock> stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStock([FromRoute] long id)
+        {
+            Stock stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+
+        }
+    }
+}
