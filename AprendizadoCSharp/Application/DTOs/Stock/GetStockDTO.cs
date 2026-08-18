6f7c24d5f89c7eb54eafea193ffd6415943a5233
JoaoPauloLordelo <jpaulovivas@gmail.com>
2026-08-18 15:40:30 -0300

    feat(application): adição de mappers e getDTO para Stock

---
+++ AprendizadoCSharp/Application/DTOs/Stock/GetStockDTO.cs
@@ -0,0 +1,17 @@
+﻿using AprendizadoCSharp.Domain.Stock.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AprendizadoCSharp.Application.DTOs.Stock
+{
+    public record GetStockDTO
+    {
+        public long Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+        public decimal Purchase { get; set; }
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+    }
+}
---
+++ AprendizadoCSharp/Application/Mappers/StockMappers.cs
@@ -0,0 +1,22 @@
+﻿using AprendizadoCSharp.Application.DTOs.Stock;
+using AprendizadoCSharp.Domain.Stock.Models;
+
+namespace AprendizadoCSharp.Application.Mappers
+{
+    public static class StockMappers
+    {
+        public static GetStockDTO toGetStockDTO(this Stock stockModel)
+        {
+            return new GetStockDTO
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
